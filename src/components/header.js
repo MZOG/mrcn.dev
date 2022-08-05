1d5d061c4ef67aef9327e8ad7f8db05095165897
Marcin <kontakt@marcinzogrodnik.pl>
2022-08-05 20:38:34 +0200

    Update

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,7 @@
 import * as React from "react"
 
 import Container from "./container"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 const Header = () => (
   <header className="py-5">
