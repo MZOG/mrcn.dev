4df2926e382378295c18e0683863799775328a33
Marcin Zogrodnik <kontakt@marcinzogrodnik.pl>
2024-05-08 14:14:32 +0200

    new projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,41 @@
-import Link from "next/link";
 import { Github, Linkedin, ExternalLink } from "lucide-react";
+import { cn } from "../../@/lib/utils";
 
 const projects = [
   {
     name: "react devs hub",
     description: "reversed job board for react developers",
     url: "https://reactdevshub.com",
+    personal: true,
+    year: 2024,
   },
   {
     name: "mediwell",
     description: "medical web development services",
     url: "https://mediwell.pl",
+    personal: true,
+    year: 2024,
   },
   {
     name: "doradcajezykowy",
     description: "website for language advisor",
     url: "https://doradcajezykowy.com",
+    personal: false,
+    year: 2023,
+  },
+  {
+    name: "raypath",
+    description: "website for raypath advisors",
+    url: "https://raypath.expert",
+    personal: false,
+    year: 2023,
+  },
+  {
+    name: "barber wizaa",
+    description: "website for local barber",
+    url: "https://barber-wizaa.pl",
+    personal: false,
+    year: 2023,
   },
 ];
 
@@ -35,18 +55,31 @@ export default function Home() {
 
       <h2 className="py-5 font-mono">projects</h2>
 
-      <ul className="space-y-4 pt-3">
+      <ul className="space-y-6 pt-3">
         {projects.map((project, index) => {
           return (
             <li key={index}>
               <a
                 href={project.url}
-                className="flex flex-col space-y-1.5 !no-underline group"
+                className="flex flex-col space-y-1.5 group"
                 target="_blank"
               >
                 <div className="">
-                  <div className="flex gap-3 items-center font-medium text-sm underline underline-offset-4">
-                    {project.name}
+                  <div className="flex gap-3 items-center font-medium text-sm">
+                    <span className="underline underline-offset-4">
+                      {project.name}
+                    </span>
+                    <div
+                      className={cn(
+                        project.personal ? "bg-blue-600" : "bg-primary",
+                        "text-[10px] text-white px-1.5 py-0 rounded-full"
+                      )}
+                    >
+                      {project.personal ? "personal" : "client"}
+                    </div>
+                    <p className="text-[10px] text-muted-foreground">
+                      {project.year}
+                    </p>
                     <ExternalLink
                       size={13}
                       className="hidden group-hover:block"
